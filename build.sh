a9315f930958de5c065ad19e11088b568dbbe4fa
madwind76 <madwind76@gmail.com>
2024-01-30 13:31:54 +0900

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 echo "Torghost installer v3.0"
 echo "Installing prerequisites "
+sudo apt-get update
 sudo apt-get install tor python3-pip python3-dev -y 
 echo "Installing dependencies "
 sudo pip3 install -r requirements.txt 
